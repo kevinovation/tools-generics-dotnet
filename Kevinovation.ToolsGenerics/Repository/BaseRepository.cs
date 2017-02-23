ea8ec4adba0c349bbf21c550b8be3da4a8058084
Kevin Blenner <k.blenner4@gmail.com>
2017-02-23 10:20:58 +0100

    socle

---
+++ Kevinovation.ToolsGenerics/Repository/BaseRepository.cs
@@ -0,0 +1,71 @@
+﻿using Kevinovation.ToolsGenerics.Models.Interface;
+using Kevinovation.ToolsGenerics.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kevinovation.ToolsGenerics.Repository
+{
+    public abstract class BaseRepository<M> : IBaseRepository<M> where M : class, IEntity
+    {
+        protected DbContext Db { get; }
+        private DbSet<M> table = null;
+
+        protected DbSet<M> Table
+        {
+            get
+            {
+                return this.table;
+            }
+        }
+
+        public BaseRepository(DbContext dbContext)
+        {
+            this.Db = dbContext;
+            this.table = Db.Set<M>();
+        }
+
+        public void Delete(int id)
+        {
+            M existing = this.SelectByID(id);
+            if (existing != null)
+                this.table.Remove(existing);
+        }
+
+        public void Delete(M m)
+        {
+            this.table.Remove(m);
+            this.Save();
+        }
+
+        public void Insert(M obj)
+        {
+            this.table.Add(obj);
+            this.Save();
+        }
+
+        public void Save()
+        {
+            this.Db.SaveChanges();
+        }
+
+        public IEnumerable<M> SelectAll()
+        {
+            return table.ToList();
+        }
+
+        public M SelectByID(int id)
+        {
+            return table.FirstOrDefault(m => m.Id == id);
+        }
+
+        public void Update(M obj)
+        {
+            table.Attach(obj);
+            Db.Entry(obj).State = EntityState.Modified;
+            this.Save();
+        }
+    }
+}
---
+++ Kevinovation.ToolsGenerics/Repository/Interface/IBaseRepository.cs
@@ -0,0 +1,23 @@
+﻿using Kevinovation.ToolsGenerics.Models.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kevinovation.ToolsGenerics.Repository.Interface
+{
+    public interface IBaseRepository<M> where M : class, IEntity
+    {
+        IEnumerable<M> SelectAll();
+
+        M SelectByID(int id);
+
+        void Insert(M obj);
+
+        void Update(M obj);
+
+        void Delete(int id);
+
+        void Save();
+    }
+}
